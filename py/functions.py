0c0dfbf7a2b8914b5a98669896e788dc551b1162
Cameron Luce <up2121518@myport.ac.uk>
2025-11-21 13:13:55 +0000

    made changes to dependencies

--- py/functions.py
+++ py/functions.py
@@ -1,16 +1,14 @@
 #imports
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+import kagglehub
+from kagglehub import KaggleDatasetAdapter
+
 
 #all helper functions used
+url = "https://www.kaggle.com/api/v1/datasets/download/bryancimo/sdss-galaxy-classification-dr18"
 
 def data_grabber():
-    data = pd.read_csv("sdss_100k_galaxy_classification_dr18.csv")
+    data = pd.read_csv(url)
     return data
-
-data = data_grabber()
-
-pd.set_option('display.max_columns', None)
-pd.set_option('display.max_rows', None)
-
-data.head()
